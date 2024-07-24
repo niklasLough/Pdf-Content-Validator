a965d0c5fb2df495cd70f5b55c26abf245dafe2d
Niklas_Lough <niklaslough99@icloud.com>
2024-07-24 16:35:36 +0200

    Added upload_pdf function

--- pdfValidator.py
+++ pdfValidator.py
@@ -1,4 +1,10 @@
 #Program to validate the presence of specific elements within a PDF file
 
-import PyPDF2
 import re
+#import PyPDF2
+#import pdfminer.six
+
+# Function to upload a PDF file to the application
+def upload_pdf():
+    # Upload a PDF file
+    print("Upload a PDF file")
