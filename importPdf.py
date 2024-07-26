fdbc09285c9092f19be4ceaf621a60c04f2a7747
Niklas_Lough <niklaslough99@icloud.com>
2024-07-26 15:11:29 +0200

    File being uploaded successfully- need to now be able to access it

--- importPdf.py
+++ importPdf.py
@@ -1,29 +1,40 @@
-# File to import a PDF file from the user
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
+from werkzeug.utils import secure_filename
+import os 
+from wtforms.validators import InputRequired
 
-# def upload_pdf():
-#     filename = "ResConfirmFr1.pdf"
-#     # Upload a PDF file
-#     print("Upload a PDF file")
-#     # Functionality
-#     return filename
-
-class UploadFileFlaskForm(FlaskForm):
-    pdf = FileField("Choose File")
-    submit = SubmitField("Upload File")
-
+# Function to upload a PDF file
 def upload_pdf():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'secret'
+    app.config['UPLOAD_FOLDER'] = os.path.join('static', 'Files')
+
+    # Ensure the upload folder exists
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+
+    class UploadFileFlaskForm(FlaskForm):
+        pdf = FileField("Choose File", validators=[InputRequired()])
+        submit = SubmitField("Upload File")
+
     @app.route('/', methods=['GET', 'POST'])
     @app.route('/home', methods=['GET', 'POST'])
-
     def home():
         form = UploadFileFlaskForm()
-        return render_template("index.html", form=form)
-    
-    app.run(debug=True) 
+        if form.validate_on_submit():
+            pdf = form.pdf.data
+            file_name = secure_filename(pdf.filename) # Secure the filename to be then saved
+            try:
+                file_path = os.path.join(app.config['UPLOAD_FOLDER'], file_name)
+                pdf.save(file_path) # Save the file
+                return redirect(url_for('home', success=True)) # Redirect to the home route
+            except Exception as e:
+                return str(e)
+        return render_template("index.html", form=form, success=request.args.get('success'))
+
+    app.run(debug=True)
 
 if __name__ == "__main__":
     upload_pdf()
--- pdfValidator.py
+++ pdfValidator.py
@@ -8,21 +8,19 @@ from pdfminer.high_level import extract_text as pdfminer_extract_text # For extr
 #Importing other files
 from importPdf import upload_pdf # For importing a PDF file from the user
 
-
 # Function to extract text from a PDF file
 def extract_pdf_text(pdf_file):
     # Extract text from a PDF file using pdfminer
     text = pdfminer_extract_text(pdf_file)
+    text = extract_pdf_text(pdf_file)
     return text
 
 
-# Main function
 def main():
     pdf_file = upload_pdf() 
     text = extract_pdf_text(pdf_file)
     print(text)
     return
-
 
 if __name__ == "__main__":
     main()
