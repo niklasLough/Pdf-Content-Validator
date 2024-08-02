200172857cc3d3802422628c85aa0c5007a24f87
Niklas_Lough <niklaslough99@icloud.com>
2024-08-02 13:48:34 +0200

    Keyword and value occurences are highlighted

--- app.py
+++ app.py
@@ -9,12 +9,14 @@ from configApp import Config
 from pdfValidator import validate_pdf
 from highlighPdf import highlight_pdf
 
+
 class UploadFileFlaskForm(FlaskForm):
     """
     Class to create a FlaskForm for uploading a PDF file
     """
     pdf = FileField("Choose File", validators=[InputRequired()])
     submit = SubmitField("Upload File")
+
 
 class InputDataFlaskForm(FlaskForm):
     """
@@ -36,6 +38,60 @@ def create_app():
     app.config.from_object(Config)
     Config.initialise_app(app)
 
+    def file_upload(upload_form):
+        pdf = upload_form.pdf.data
+        file_name = secure_filename(pdf.filename)
+        print(file_name)
+        if not file_name.endswith('.pdf'):
+            print("hello")
+            return redirect(url_for('home', not_pdf=True))
+        try:
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], file_name)
+            pdf.save(file_path)
+
+            # Save the file path in a session to use across different requests
+            session['file_path'] = file_path
+
+            return redirect(url_for('home', success=True))
+        except Exception as e:
+            return str(e)
+
+
+    def handle_input(input_form):
+        # Get the user input
+        keyword = input_form.keyword.data
+        value = input_form.value.data
+
+        # Get the file path from the session
+        pdf_file_path = session.get('file_path')
+        found = False
+        found, price_valid = validate_pdf(pdf_file_path, keyword, value)
+        if found:
+            highlight_pdf(pdf_file_path, keyword, value)
+
+        session['found'] = found
+        session['price_valid'] = price_valid
+        if found and price_valid:
+            print("found and price_valid")
+            return redirect(url_for('home', success=True, validated=True, price_valid=True))
+        elif found and price_valid is None:
+            print("found")
+            return redirect(url_for('home', success=True, validated=True))
+        elif not found and price_valid is None:
+            print("not found")
+            return redirect(url_for('home', success=True, not_validated=True))
+        elif found and price_valid is False:
+            print("found and price_invalid")
+            return redirect(url_for('home', success=True, validated=True, price_invalid=True))
+        elif not found and price_valid:
+            print("price_valid")
+            return redirect(url_for('home', success=True, not_validated=True, price_valid=True))
+        else:
+            print("noooo")
+            return redirect(url_for('home', success=True, not_validated=True, price_invalid=True))
+
+
+
     @app.route('/', methods=['GET', 'POST'])
     @app.route('/home', methods=['GET', 'POST'])
     def home():
@@ -44,55 +100,12 @@ def create_app():
 
         # If a file is uploaded
         if upload_form.validate_on_submit():
-            pdf = upload_form.pdf.data
-            file_name = secure_filename(pdf.filename)
-            try:
-                file_path = os.path.join(app.config['UPLOAD_FOLDER'], file_name)
-                pdf.save(file_path)
-
-                # Save the file path in a session to use across different requests
-                session['file_path'] = file_path
-
-                return redirect(url_for('home', success=True))
-            except Exception as e:
-                return str(e)
+            return file_upload(upload_form)
         
         # If input data is submitted
         if input_form.validate_on_submit():
-            # Get the user input
-            keyword = input_form.keyword.data
-            value = input_form.value.data
-
-            # Get the file path from the session
-            pdf_file_path = session.get('file_path')
-            found = False
-            found, price_valid = validate_pdf(pdf_file_path, keyword, value)
-            # if found:
-            #     new_file_path = highlight_pdf(pdf_file_path, keyword, value)
-            #     session['file_path'] = new_file_path
-
-            session['found'] = found
-            session['price_valid'] = price_valid
-            if found and price_valid:
-                print("found and price_valid")
-                return redirect(url_for('home', success=True, validated=True, price_valid=True))
-            elif found and price_valid is None:
-                print("found")
-                return redirect(url_for('home', success=True, validated=True))
-            elif not found and price_valid is None:
-                print("not found")
-                return redirect(url_for('home', success=True, not_validated=True))
-            elif found and price_valid is False:
-                print("found and price_invalid")
-                return redirect(url_for('home', success=True, validated=True, price_invalid=True))
-            elif not found and price_valid:
-                print("price_valid")
-                return redirect(url_for('home', success=True, not_validated=True, price_valid=True))
-            else:
-                print("noooo")
-                return redirect(url_for('home', success=True, not_validated=True, price_invalid=True))
+            return handle_input(input_form)
         
-
         # found = session.get('found')
         # price_valid = session.get('price_valid')
         pdf_file_path = session.get('file_path')
@@ -105,6 +118,7 @@ def create_app():
                             not_validated=request.args.get('not_validated'),
                             price_invalid=request.args.get('price_invalid'), 
                             price_valid=request.args.get('price_valid'),
+                            not_pdf=request.args.get('not_pdf'),
                             pdf_file_path=pdf_file_path)
 
     @app.route('/help')
@@ -113,6 +127,7 @@ def create_app():
 
     app.run(debug=True)
     return app
+
 
 if __name__ == "__main__":
     create_app()
--- highlighPdf.py
+++ highlighPdf.py
@@ -1,10 +1,34 @@
-import pymupdf
+import fitz  # PyMuPDF
+import os
 
 def highlight_pdf(file_path, keyword, value):
-    highlighted_file =pymupdf.open(file_path)
-    for page in highlighted_file:
-        text = page.get_text()
-        if keyword in text and value in text:
-            page.add_highlight_annot((0, 0, 0), text.find(keyword), text.find(value), text.find(value) + len(value))
+    """
+    Highlight text in a PDF file and save to the same file.
 
-    return highlighted_file
+    Args:
+    file_path (str): Path to the input PDF file.
+    keyword (str): Text to highlight.
+
+    Returns:
+    str: Path to the output PDF file.
+    """
+    doc = fitz.open(file_path)
+    for page_num in range(doc.page_count):
+        page = doc.load_page(page_num)
+        text_instances = page.search_for(keyword.lower())
+        for inst in text_instances:
+            highlight = page.add_highlight_annot(inst)
+            highlight.update()
+        text_instances = page.search_for(value.lower())
+        for inst in text_instances:
+            highlight = page.add_highlight_annot(inst)
+            highlight.update()
+
+    # Save the document to a temporary file
+    temp_file_path = file_path + '.highlight'
+    doc.save(temp_file_path, garbage=4, deflate=True)
+    doc.close()
+
+    # Replace the original file with the temporary file
+    os.replace(temp_file_path, file_path)
+    return file_path
