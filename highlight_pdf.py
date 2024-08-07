dd5ef7120c8908dd9029b7beed1756f2ec53cb18
Niklas_Lough <niklaslough99@icloud.com>
2024-08-07 13:12:34 +0200

    Previous highlights are now removed when a new validation is made

--- highlight_pdf.py
+++ highlight_pdf.py
@@ -16,6 +16,7 @@ def highlight_pdf(file_path, keyword, value):
     doc = fitz.open(file_path)
     for page_num in range(doc.page_count):
         page = doc.load_page(page_num)
+        page = clear_highlights(page)
         text_instances = page.search_for(keyword.lower())
         for inst in text_instances:
             highlight = page.add_highlight_annot(inst)
@@ -33,3 +34,20 @@ def highlight_pdf(file_path, keyword, value):
     # Replace the original file with the temporary file
     os.replace(temp_file_path, file_path)
     return file_path
+
+
+def clear_highlights(page):
+    """
+    Clear all highlights in the PDF file
+    
+    Args:
+    page: fitz.Page: The page to clear the highlights from
+    """
+    annotations = page.annots()
+    if annotations:
+        print("Hello")
+        for annot in annotations:
+            print("Hello from for loop")
+            if annot.type[0] == 8:  # Type 8 means the annotations is a highlight
+                page.delete_annot(annot)
+    return page
